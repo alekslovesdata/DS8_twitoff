fc7978e8f14214d6024cd877aa458e0e54bb2c17
alekslovesdata <alekslovesdata@users.noreply.github.com>
2019-11-13 09:55:04 -0800

    added stuff to base.html template

--- TWITOFF/twitter.py
+++ TWITOFF/twitter.py
@@ -27,7 +27,8 @@ def add_or_update_user(username):
         for tweet in tweets:
             #calculate embedding on the full tweet
             embedding = BASILICA.embed_sentence(tweet.full_text, model='twitter')
-            db_tweet = Tweet(id=tweet.id, text=tweet.full_text[:300], embedding=embedding)
+            db_tweet = Tweet(id=tweet.id, text=tweet.full_text[:300],
+            embedding=embedding)
             db_user.tweets.append(db_tweet)
             DB.session.add(db_tweet)
     except Exception as e:
